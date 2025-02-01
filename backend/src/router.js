28959de50e250644cfe78a0453f565d01d558d53
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2025-01-31 19:28:26 -0500

    create folders for frontend and backend

--- backend/src/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const streamRouter = require('./routes/stream');
-const userRouter = require('./routes/test');
-
-router.use('/stream', streamRouter);
-router.use('/test', userRouter);
-
-module.exports = router;
